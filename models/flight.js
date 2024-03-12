d13c97cf7fc3f1726cd2f8aa111ec66cbbdcb692
jamesonhussey <jamesonhussey99@gmail.com>
2024-03-12 15:35:54 -0500

    added destination schema & integrated it into Flight model

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    },
+    arrival: {
+        type: Date,
+    }
+})
+
 const flightSchema = new Schema({
     airline: {
         type: String,
@@ -21,6 +31,9 @@ const flightSchema = new Schema({
         type: Date,
         default: '2025-03-11'
     },
+    destinations: {
+        type: [destinationSchema],
+    }
 }, {
     timestamps:true
 })
